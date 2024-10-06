0e840acbe4b83406dbc141cc177d28527b172943
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-06 18:34:00 +0530

    AnonymusObject

---
+++ AnonymusObject.java
@@ -0,0 +1,12 @@
+//Anonymus object means object is created without reference Variable
+//It is accessible only once only
+public class AnonymusObject {
+    void display(){
+        System.out.println("hello i am display method");
+    }
+    public static void main(String[] args) {
+     //It is the Anonymus Object
+        new ObjectCreation().display();
+  
+    }
+}
