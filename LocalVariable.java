ee4bc0c551753a5405dd1998f625d6dd28ea6893
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-06 18:55:58 +0530

    Local variables

---
+++ LocalVariable.java
@@ -0,0 +1,10 @@
+//A variable that is defined inside a method
+//memory allocated to local variable when the method is called
+//local variables are stored in the stack area
+//local variable cannot be static
+public class LocalVariable {
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println(x);
+    }
+}
