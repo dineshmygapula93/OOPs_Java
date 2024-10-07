2b9d57ca0eb56febbd7fc6169db9ca69559b5098
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-07 18:08:35 +0530

    about Constructor

---
+++ Constructors.java
@@ -0,0 +1,11 @@
+//A constructor is the special method which has same name as the class name & which has no return type
+//Constructor is called automatically when is object is created
+//Constructors are used to initialize instance variables
+public class Constructors {
+   Constructors(){
+      System.out.println("hello ");
+   } 
+   public static void main(String[] args) {
+   new Constructors();
+   }
+}
