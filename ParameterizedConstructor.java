38fc3e7e5075aa0e31e9e4032c27bb746c5bf666
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-07 18:56:10 +0530

    Parameterized Constructor

---
+++ ParameterizedConstructor.java
@@ -0,0 +1,9 @@
+//The Constructor having parameters is called ParameterizedConstructor
+public class ParameterizedConstructor {
+    ParameterizedConstructor(int a){
+            System.out.println(a);
+        }
+        public static void main(String[] args) {
+            new ParameterizedConstructor(10);
+        }
+}
