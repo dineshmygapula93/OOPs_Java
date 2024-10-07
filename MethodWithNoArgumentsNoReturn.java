326780b4dcd8682b338ab1ffb1dbbec0e3a05c08
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-07 12:04:44 +0530

    MethodWithout Arguments &No Return value

--- MethodWithNoArgumentsNoReturn.java
+++ MethodWithNoArgumentsNoReturn.java
@@ -1,6 +1,6 @@
-//Method with no arguments and no return type
+//Method with no arguments and no return value
 public class MethodWithNoArgumentsNoReturn {
-    //the display method as no arguments and no return type
+    //the display method as no arguments and no return value
     void display(){
         System.out.println("hello i am dinesh");
     }
