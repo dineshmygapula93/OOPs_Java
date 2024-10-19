9c15dd8b97df8ee48321725924f60ce2390a81e8
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-19 12:52:16 +0530

    String parsing

---
+++ 2.ObjectOrientedProgramming/2.2.Variables,Methods&Constructors/stringParseInt.java
@@ -0,0 +1,9 @@
+//parse is used to convert String to int or any requried numbers data type
+public class stringParseInt {
+    public static void main(String[] args) {
+        //Integer is classes and parseInt is static method in it
+        int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        System.out.println(x+y);
+    }
+}
