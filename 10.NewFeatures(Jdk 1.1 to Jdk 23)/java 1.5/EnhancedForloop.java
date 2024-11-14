b8baa981806f10aa6e7b840e5a3b8a23984e6b57
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2024-11-14 17:31:36 +0530

    EnhancedForloop

---
+++ 10.NewFeatures(Jdk 1.1 to Jdk 23)/java 1.5/EnhancedForloop.java
@@ -0,0 +1,10 @@
+//EnchancedForLoop :-
+public class EnhancedForloop {
+
+    public static void main(String[] args) {
+        int a[]={2,4,6,3,23,234,};
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
