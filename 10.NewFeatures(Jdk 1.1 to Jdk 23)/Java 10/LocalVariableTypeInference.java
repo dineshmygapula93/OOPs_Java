0066d5bdea3768fda9774243e8f59d202df95dfa
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2024-12-30 16:50:15 +0530

    LocalVariableTypeInference

--- 10.NewFeatures(Jdk 1.1 to Jdk 23)/Java 10/LocalVariableTypeInference.java
+++ 10.NewFeatures(Jdk 1.1 to Jdk 23)/Java 10/LocalVariableTypeInference.java
@@ -1,5 +1,5 @@
 /*Local Variable Type Inference:-
-                This feature allows to store any type of the data in a var type variable.
+ *              This feature allows to store any type of the data in a var type variable.
  *              This feature  can be used to local Variable only
  */
 public class LocalVariableTypeInference {
